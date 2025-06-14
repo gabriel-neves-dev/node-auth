4b53cf821730196c2715dadeddd4f0dcd70e51c2
Gabriel Neves <gabrielneves.dev@gmail.com>
2025-06-14 00:21:34 -0300

    added routes from application

--- server.js
+++ server.js
@@ -5,8 +5,21 @@ const PORT = process.env.PORT || 4000;
 
 
 app.get('/', (req, res) => {
-    res.send('Hello')
+    res.render('index');
 });
+
+app.get('/users/register', (req, res) => {
+    req.render('register')
+});
+
+app.get('/users/login', (req, res) => {
+    req.render('login')
+});
+
+app.get('/users/dashboard', (req, res) => {
+    req.render('dashboard')
+});
+
 
 
 app.listen(PORT, () => {
