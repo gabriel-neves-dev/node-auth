38e1605ce84f8ac4e2269eb19d4c1a2cca4b8c03
Gabriel Neves <gabrielneves.dev@gmail.com>
2025-06-20 15:34:59 -0300

    reajuste das views

--- src/server.js
+++ src/server.js
@@ -7,9 +7,15 @@ const bcrypt = require("bcrypt");
 const session = require("express-session");
 const flash = require("express-flash");
 const passport = require("passport");
+const { checkAuthenticated, checkNotAuthenticaded } = require("./middlewares/authMiddlewares.js");
+
+
 
 const PORT = process.env.PORT || 4000;
 
+const path = require("path");
+
+app.set("views", path.join(__dirname, "views")); // ou ajuste para o caminho correto
 app.set("view engine", "ejs"); //middleware
 
 app.use(express.urlencoded({ extended: false }));
@@ -33,133 +39,17 @@ app.use(passport.session());
 
 app.use(flash());
 
-app.get("/", (req, res) => {
-  res.render("index");
-});
+const registerRouter = require("./routes/register.js");
+app.use("/users/register", registerRouter);
+const loginRouter = require("./routes/login.js");
+app.use("/users/login", loginRouter);
+const logoutRouter = require("./routes/logout.js");
+app.use("/users/logout", logoutRouter);
+const dashboardRouter = require("./routes/dashboard.js");
+app.use("/users/dashboard", dashboardRouter);
+const indexRouter = require("./routes/index.js");
+app.use("/", indexRouter);
 
-app.get("/users/register", checkAuthenticated, (req, res) => {
-  res.render("register");
-});
-
-app.get("/users/login", checkAuthenticated, (req, res) => {
-  res.render("login");
-});
-
-app.get("/users/dashboard", checkNotAuthenticaded, (req, res) => {
-  res.render("dashboard", { user: req.user.name });
-});
-
-app.get("/users/logout", (req, res) => {
-  req.logout((err) => {
-    if (err) {
-      console.error("Erro ao fazer logout", err);
-      return res.status(500).send("Erro durante logout");
-    }
-
-    req.session.destroy((err) => {
-      if (err) {
-        console.error("Erro ao destruir sessão:", err);
-        return res.status(500).send("Erro ao destruir sessão");
-      }
-      res.redirect("/");
-    });
-  });
-});
-
-app.post("/users/register", async (req, res) => {
-  let { name, email, password, password2 } = req.body;
-
-  console.log({
-    name,
-    email,
-    password,
-    password2,
-  });
-
-  let errors = [];
-
-  if (!name || !password || !password2) {
-    errors.push({ message: "Preencha todos os campos!" });
-  }
-
-  if (password.length < 6) {
-    errors.push({ message: "Senha deve ter mais de 6 caracteres." });
-  }
-
-  if (password != password2) {
-    errors.push({ message: "As senhas devem ser iguais." });
-  }
-
-  if (errors.length > 0) {
-    res.render("register", { errors });
-  } else {
-    // form vlaidation has passed
-
-    let hashedPassword = await bcrypt.hash(password, 10);
-    console.log(hashedPassword);
-
-    pool.query(
-      `SELECT * FROM users
-      WHERE email = $1`,
-      [email],
-      (err, results) => {
-        if (err) {
-          throw err;
-        }
-        console.log(results.rows);
-
-        if (results.rows.length > 0) {
-          errors.push({ message: "Email already registered" });
-          res.render("register", { errors });
-        } else {
-          pool.query(
-            `INSERT INTO users (name, email, password)
-            VALUES ($1, $2, $3)
-            RETURNING id, password`,
-            [name, email, hashedPassword],
-            (err, results) => {
-              if (err) {
-                throw err;
-              }
-
-              console.log(results.rows);
-              req.flash(
-                "sucess_msg",
-                "Registro completo com sucesso! Por favor faça o login"
-              );
-              res.redirect("/users/login");
-            }
-          );
-        }
-      }
-    );
-  }
-});
-
-app.post(
-  "/users/login",
-  passport.authenticate("local", {
-    successRedirect: "/users/dashboard",
-    failureRedirect: "/users/login",
-    failureFlash: true,
-  })
-);
-
-//MIDDLEWARES
-function checkAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) {
-    return res.redirect("/users/dashboard")
-  }
-  next()
-}
-
-function checkNotAuthenticaded(req, res, next){
-  if (req.isAuthenticated()) {
-    return next()
-  }
-
-  res.redirect("/users/login")
-}
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
