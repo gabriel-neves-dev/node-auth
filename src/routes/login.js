7b3596a31ff145b68dc8f2e72569f9e15b8577e4
Gabriel Neves <gabrielneves.dev@gmail.com>
2025-06-20 15:30:57 -0300

    rota login  separada de server.js

---
+++ src/routes/login.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
+const { checkAuthenticated, checkNotAuthenticaded } = require('../middlewares/authMiddlewares.js');
+
+
+router.get("/", checkAuthenticated, (req, res) => {
+  res.render("login");
+});
+
+router.post(
+  "/",
+  passport.authenticate("local", {
+    successRedirect: "/users/dashboard",
+    failureRedirect: "/users/login",
+    failureFlash: true,
+  })
+);
+module.exports = router;
