d11cf0cb97d403eb6b6eb13adbfc995c7778a30f
Gabriel Neves <gabrielneves.dev@gmail.com>
2025-06-25 18:39:26 -0300

    added jwt strategy to passport

--- src/passportConfig.js
+++ src/passportConfig.js
@@ -1,59 +1,30 @@
-const LocalStrategy = require("passport-local").Strategy;
 const { pool } = require("./dbConfig");
 const bcrypt = require("bcrypt");
+const passport = require("passport");
+const JwtStrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
+const userService = require("./services/userService");
 
 function initialize(passport) {
-  const authenticateUser = (email, password, done) => {
-    pool.query(
-      `SELECT * FROM users WHERE email = $1`,
-      [email],
-      (err, results) => {
-        if (err) {
-          throw err;
-        }
+  // Configuração da estratégia JWT
 
-        console.log(results.rows);
-        if (results.rows.length > 0) {
-          const user = results.rows[0];
-
-          bcrypt.compare(password, user.password, (err, isMatch) => {
-            if (err) {
-              console.log(err);
-            }
-
-            if (isMatch) {
-              return done(null, user);
-            } else {
-              return done(null, false, { message: "Senha incorreta" });
-            }
-          });
-        } else {
-          return done(null, false, { message: "Email não registrado." });
-        }
-      }
-    );
+  const opts = {
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(), // extrai o token do cabeçalho Authorization
+    secretOrKey: process.env.JWT_SECRET, // chave secreta para verificar o token
   };
   passport.use(
-    new LocalStrategy(
-      {
-        usernameField: "email",
-        passwordField: "password",
-      },
-      authenticateUser
-    )
-  );
-
-  passport.serializeUser((user, done) => done(null, user.id));
-
-  passport.deserializeUser((id, done) => {
-    pool.query(`SELECT * FROM users WHERE id = $1`, [id], (err, results) => {
-      if (err) {
-        return done(err);
+    new JwtStrategy(opts, async (jwt_payload, done) => {
+      try {
+        const user = await userService.findUserById(jwt_payload.id);
+        if (!user)
+          return done(null, false, { message: "Usuário não encontrado" });
+        return done(null, user);
+      } catch (err) {
+        console.error("Erro ao buscar usuário:", err);
+        return done(err, false);
       }
-    //   console.log(`ID is ${results.rows[0].id}`);
-      return done(null, results.rows[0]);
-    });
-  });
+    })
+  );
 }
 
 module.exports = initialize;
